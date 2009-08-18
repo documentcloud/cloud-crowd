135ce099c2e6126117f702a46f854ebccc09fe01
Jeremy Ashkenas <jashkenas@gmail.com>
2009-08-18 16:36:34 -0400

    commenting Rails models/controllers

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,10 +1,15 @@
+# The Dogpile API. You can post a new job in either XML or JSON format, 
+# and check on the status of your job by getting its id.
 class JobsController < ApplicationController
   
+  # Begin a new job. This will populate the queue with component WorkUnits.
   def create
     job = Job.create_from_request(JSON.parse(params[:json]))
     render :json => job
   end
   
+  # Check on the status of a job. If the job has completed, the response will
+  # include all of the output.
   def show
     job = Job.find(params[:id])
     render :json => job
--- app/controllers/work_units_controller.rb
+++ app/controllers/work_units_controller.rb
@@ -1,21 +1,25 @@
+# This controller is the interface between the pack of Daemons and the 
+# central server. Daemons periodially call +fetch+ to check for pending 
+# WorkUnits. When they're done processing a unit, they post back to +finish+,
+# if the unit has been successfully processed, otherwise +fail+.
 class WorkUnitsController < ApplicationController
   
-  # Ditto from below.
+  # Check to see if there's any work that needs to be done. Take it if there is.
   def fetch
     unit = WorkUnit.first(:conditions => {:status => Dogpile::PENDING}, :order => "created_at desc", :lock => true)
     return head :no_content unless unit
-    unit.status = Dogpile::PROCESSING
-    unit.save!
+    unit.update_attributes(:status => Dogpile::PROCESSING)
     render :json => unit
   end
   
-  # Perhaps move this into a WorkUnit class method with a transaction.
+  # When a WorkUnit has finished processing, mark it as successful.
   def finish
     unit = WorkUnit.find(params[:id], :lock => true)
     unit.update_attributes(:output => params[:output], :status => Dogpile::SUCCEEDED, :time => params[:time])
     head :no_content
   end
   
+  # When a WorkUnit has failed to process, mark it as failed.
   def fail
     unit = WorkUnit.find(params[:id], :lock => true)
     unit.update_attributes(:output => params[:output], :status => Dogpile::FAILED, :time => params[:time])
