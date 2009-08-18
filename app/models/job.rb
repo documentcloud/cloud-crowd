ba06618df05b98612bca05300d1fa8c226e487ac
Jeremy Ashkenas <jashkenas@gmail.com>
2009-08-18 12:22:33 -0400

    Added functional ETA for jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -19,7 +19,35 @@ class Job < ActiveRecord::Base
   
   def queue_for_daemons
     JSON.parse(self.inputs).each do |wu_input|
-      WorkUnit.create(:job => self, :input => wu_input, :status => Dogpile::WAITING)
+      WorkUnit.create(:job => self, :input => wu_input, :status => Dogpile::PENDING)
+    end
+  end
+    
+  def check_for_completion
+    update_attributes(:status => Dogpile::COMPLETE) if all_work_units_done?
+  end
+  
+  def all_work_units_done?
+    self.work_units.incomplete.count <= 0
+  end
+  
+  def eta
+    num_remaining = self.work_units.incomplete.count
+    return 0 if num_remaining <= 0
+    done_units = self.work_units.done
+    return nil if done_units.empty?
+    avg_time = done_units.inject(0) {|sum, unit| sum + unit.time } / done_units.length
+    avg_time * num_remaining
+  end
+  
+  def display_eta
+    time = self.eta
+    return "unknown" if !time
+    return "completed" if time == 0
+    case time
+    when (0..(1.minute))        then "#{time} seconds"
+    when ((1.minute)..(1.hour)) then "#{time / 1.minute} minutes"
+    else                             "#{time / 1.hour} hours"
     end
   end
   
@@ -29,9 +57,10 @@ class Job < ActiveRecord::Base
     outs  = units.inject({}) {|memo, u| memo[u.input] = u.output if u.done?; memo }
     {
       'id'        => self.id,
-      'status'    => self.status,
+      'status'    => Dogpile.display_status(self.status),
       'inputs'    => ins,
-      'outputs'   => outs
+      'outputs'   => outs,
+      'eta'       => self.display_eta
     }.to_json
   end
   
