4da9fd1e00660922d642ae43830e6e3496f57fba
Jeremy Ashkenas <jashkenas@gmail.com>
2009-08-20 13:33:01 -0400

    switching to MySQL and adding transactions

--- examples/dc_import.rb
+++ examples/dc_import.rb
@@ -3,7 +3,7 @@ require 'json'
 pdfs = Dir['/Users/jashkenas/Desktop/document_cloud/*.pdf'].map {|pdf| "file://#{pdf}"}
 
 RestClient.post(
-	'http://localhost:3000/jobs', 
+	'http://localhost:9173/jobs', 
 	{:json => JSON.generate({
 	
 		'action' => 'document_cloud_import',
