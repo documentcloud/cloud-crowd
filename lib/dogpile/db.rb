583ec31a618820a841bb524d58cc77a97906589d
Jeremy Ashkenas <jashkenas@gmail.com>
2009-08-23 13:59:47 -0400

    database connection

---
+++ lib/dogpile/db.rb
@@ -0,0 +1,35 @@
+# Use the 'db' settings in dogpile.yml to connect to the database.
+ActiveRecord::Base.establish_connection(Dogpile.config[:db])
+
+# Complete schema for Dogpile.
+ActiveRecord::Schema.define(:version => 1) do
+
+  create_table "jobs", :force => true do |t|
+    t.integer  "status",                      :null => false
+    t.text     "inputs",                      :null => false
+    t.string   "action",                      :null => false
+    t.text     "options",                     :null => false
+    t.float    "time"
+    t.string   "callback_url"
+    t.string   "owner_email"
+    t.integer  "lock_version", :default => 0, :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "work_units", :force => true do |t|
+    t.integer  "status",                      :null => false
+    t.integer  "job_id",                      :null => false
+    t.string   "input",                       :null => false
+    t.integer  "attempts",     :default => 0, :null => false
+    t.integer  "lock_version", :default => 0, :null => false
+    t.float    "time"
+    t.text     "output"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "work_units", ["job_id"], :name => "index_work_units_on_job_id"
+  add_index "work_units", ["status"], :name => "index_work_units_on_status"
+
+end
