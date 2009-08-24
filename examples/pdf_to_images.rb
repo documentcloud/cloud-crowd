dfbda7f03ac8d86da9a37ff42fd3803df8023f98
Jeremy Ashkenas <jashkenas@gmail.com>
2009-08-24 19:20:50 -0400

    cia doc

--- examples/pdf_to_images.rb
+++ examples/pdf_to_images.rb
@@ -7,8 +7,7 @@ puts RestClient.post(
 		'action' => 'pdf_to_images',
 		
 		'inputs' => [
-		  'http://s3.amazonaws.com/dogpile_development/pdfs/bigtable-osdi06.pdf',
-		  'http://s3.amazonaws.com/dogpile_development/pdfs/DocumentCloud_As_Submitted.pdf'
+		  'http://s3.amazonaws.com/dogpile_development/pdfs/cia_rdi.pdf'
 		]
 		
 	}.to_json}
