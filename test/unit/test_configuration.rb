2a9d707428b610792311c872b0990d617afd6646
Chad Fowler <chad@chadfowler.com>
2009-10-23 14:00:23 -0600

    Don't load custom actions unless custom actions is set. Otherwise, ruby files in / are loaded, which is bad

--- test/unit/test_configuration.rb
+++ test/unit/test_configuration.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class ConfigurationTest < Test::Unit::TestCase
 
   context "CloudCrowd Configuration" do
-            
+    [:setup, :teardown].each{|hook| send(hook){clear_loaded_actions!}}
+
     should "have read in config.yml" do
       assert CloudCrowd.config[:max_workers] == 10
       assert CloudCrowd.config[:storage] == 'filesystem'
@@ -22,7 +23,33 @@ class ConfigurationTest < Test::Unit::TestCase
       assert CloudCrowd.actions['process_pdfs']     == ProcessPdfs
       assert CloudCrowd.actions['graphics_magick']  == GraphicsMagick
     end
-            
+    
+    should "not find custom actions unless CloudCrowd.config[:actions_path] is set" do
+      clear_loaded_actions!
+      CloudCrowd.config[:actions_path] = nil
+      CloudCrowd.expects(:default_actions).returns(sample_action_list)
+      CloudCrowd.expects(:installed_actions).returns(sample_action_list)
+      CloudCrowd.expects(:load_action_from).times(2)
+      CloudCrowd.actions
+    end
+    
+    should "find custom actions when CloudCrowd.config[:actions_path] is set" do
+      clear_loaded_actions!
+      CloudCrowd.config[:actions_path] = "#{File.join(File.dirname(__FILE__), "..", "config/actions")}"
+      CloudCrowd.expects(:default_actions).returns(sample_action_list)
+      CloudCrowd.expects(:installed_actions).returns(sample_action_list)
+      CloudCrowd.expects(:load_action_from).times(3)
+      CloudCrowd.actions
+    end
+         
   end
   
+  private
+    def clear_loaded_actions!
+      CloudCrowd.instance_variable_set("@actions", nil)
+    end
+    
+    def sample_action_list
+      ['hello']
+    end
 end
