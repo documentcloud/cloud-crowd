0d385425c0e424c6bd1f797493422fd2a4074fa2
Jeremy Ashkenas <jashkenas@gmail.com>
2009-12-04 19:23:21 -0500

    adding an artificial slowdown to the word count example, for demonstration purposes

--- actions/word_count.rb
+++ actions/word_count.rb
@@ -1,14 +1,16 @@
-# A parallel WordCount. Depends on the 'wc' utility. 
+# A parallel WordCount. Depends on the 'wc' utility.
 class WordCount < CloudCrowd::Action
-  
+
   # Count the words in a single book.
+  # Pretend that this takes longer than it really does, for demonstration purposes.
   def process
+    sleep 5
     (`wc -w #{input_path}`).match(/\A\s*(\d+)/)[1].to_i
   end
-  
+
   # Sum the total word count.
   def merge
     input.inject(0) {|sum, count| sum + count }
   end
-  
+
 end
