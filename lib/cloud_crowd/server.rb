93276abae788861e4d027aff4de3e9fa48f0df25
Felipe Oliveira <felipecvo@gmail.com>
2010-07-30 14:00:37 -0300

    After created job, try to distribute work units asynchronously, and return as soon as possible to client. By this way, when distribution fail or take too long, client request won't get affected.

--- lib/cloud_crowd/server.rb
+++ lib/cloud_crowd/server.rb
@@ -70,7 +70,7 @@ module CloudCrowd
     # Distributes all work units to available nodes.
     post '/jobs' do
       job = Job.create_from_request(JSON.parse(params[:job]))
-      WorkUnit.distribute_to_nodes
+      Thread.new { WorkUnit.distribute_to_nodes }
       puts "Job ##{job.id} (#{job.action}) started." unless ENV['RACK_ENV'] == 'test'
       json job
     end
