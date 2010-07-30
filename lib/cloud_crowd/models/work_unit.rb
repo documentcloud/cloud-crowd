8f02680a195fe5fce7d5fd7336d56c6bcf6072f9
Felipe Oliveira <felipecvo@gmail.com>
2010-07-30 14:16:08 -0300

    If node can't handle this action, put it back to available node list, so it can handle another action.

--- lib/cloud_crowd/models/work_unit.rb
+++ lib/cloud_crowd/models/work_unit.rb
@@ -54,6 +54,7 @@ module CloudCrowd
             unit.cancel_reservation
           end
           work_units.push(unit)
+          available_nodes.push(node)
         end
         if work_units.any? && available_nodes.any?
           filter = {:action => available_nodes.map {|node| node.actions }.flatten.uniq }
