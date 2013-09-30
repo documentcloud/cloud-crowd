3a7fabeeaa854697fc0b9c385e9712518c287155
Nathan Stitt <nathan@stitt.org>
2013-09-30 13:46:15 -0500

    Add current directory to LOAD_PATH
    If actions are not an absolute path then they need to be loaded from the
    current directory.
    
    An alternate method would be to convert relative action paths to absolute.

--- lib/cloud-crowd.rb
+++ lib/cloud-crowd.rb
@@ -97,6 +97,9 @@ module CloudCrowd
       @config_path = File.expand_path(File.dirname(config_path))
       @config = YAML.load(ERB.new(File.read(config_path)).result)
       @config[:work_unit_retries] ||= MIN_RETRIES
+      if @config[:actions_path] && Pathname.new( @config[:actions_path] ).relative? && ! $LOAD_PATH.include?( '.' )
+        $LOAD_PATH.unshift( '.' )
+      end
     end
 
     # Configure the CloudCrowd central database (and connect to it), by passing
