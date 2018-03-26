772433e1d830d6204a3a1b59cd8aa533a88a3d6a
Ted Han <ted@knowtheory.net>
2018-03-26 10:23:55 -0700

    Remove commented out blueprints.

--- test/blueprints.rb
+++ test/blueprints.rb
@@ -1,26 +1,3 @@
-# CloudCrowd::Job.blueprint do
-#   status  { CloudCrowd::PROCESSING }
-#   inputs  { ['http://www.google.com/intl/en_ALL/images/logo.gif'].to_json }
-#   action  { 'graphics_magick' }
-#   options { {}.to_json }
-#   email   { 'noone@example.com' }
-# end
-# 
-# CloudCrowd::NodeRecord.blueprint do
-#   host            { "hostname-#{sn}" }
-#   ip_address      { '127.0.0.1' }
-#   port            { 6093 }
-#   enabled_actions { 'graphics_magick,word_count' }
-#   max_workers     { 3 }
-# end
-# 
-# CloudCrowd::WorkUnit.blueprint do
-#   job     { CloudCrowd::Job.make! }
-#   status  { CloudCrowd::PROCESSING }
-#   input   { '{"key":"value"}' }
-#   action  { 'graphics_magick' }
-# end
-
 FactoryBot.define do
   factory :job, class: CloudCrowd::Job do
     status  CloudCrowd::PROCESSING
